3f6d961ae007082876d10c09e790c03782a67068
sanjay <sanjayraj9291@gmail.com>
2023-02-25 11:01:16 +0000

    galleryapp

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,109 @@
+import {Component} from 'react'
+import ThumbnailItem from '../ThumbnailItem'
+import './index.css'
+
+const imagesList = [
+  {
+    id: 0,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-thumbnail-img.png',
+    imageAltText: 'nature mountain with pond',
+    thumbnailAltText: 'nature mountain with pond thumbnail',
+  },
+  {
+    id: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-sunset-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-sunset-thumbnail-img.png',
+    imageAltText: 'nature sunset',
+    thumbnailAltText: 'nature sunset thumbnail',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-thumbnail-img.png',
+    imageAltText: 'nature mountain with ocean',
+    thumbnailAltText: 'nature mountain with ocean thumbnail',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-thumbnail-img.png',
+    imageAltText: 'nature mountain with forest',
+    thumbnailAltText: 'nature mountain with forest thumbnail',
+  },
+  {
+    id: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-leaves-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-leaves-thumbnail-img.png',
+    imageAltText: 'nature leaves',
+    thumbnailAltText: 'nature leaves thumbnail',
+  },
+  {
+    id: 5,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-tree-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-tree-thumbnail-img.png',
+    imageAltText: 'nature tree',
+    thumbnailAltText: 'nature tree thumbnail',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-thumbnail-img.png',
+    imageAltText: 'nature waterfall',
+    thumbnailAltText: 'nature waterfall thumbnail',
+  },
+  {
+    id: 7,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-river-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-river-thumbnail-img.png',
+    imageAltText: 'nature river',
+    thumbnailAltText: 'nature river thumbnail',
+  },
+]
+class Gallery extends Component {
+  state = {
+    activeTabId: imagesList[0].id,
+  }
+
+  clickTabItem = id => {
+    this.setState({activeTabId: id})
+  }
+
+  render() {
+    const {activeTabId} = this.state
+    const {imageUrl, imageAltText} = imagesList[activeTabId]
+    return (
+      <div className="bg-container">
+        <div className="column">
+          <img src={imageUrl} alt={imageAltText} className="selected-image" />
+          <h1 className="heading">Nature Photography</h1>
+          <p className="description">Nature Photography by Rahul</p>
+          <ul className="thumbnaillist">
+            {imagesList.map(eachItem => (
+              <ThumbnailItem
+                key={eachItem.id}
+                tabDetails={eachItem}
+                isActive={activeTabId === eachItem.id}
+                clickTabItem={this.clickTabItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Gallery
+// Write your code here.
